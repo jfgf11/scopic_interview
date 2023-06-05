61ac75d214705684c3342fef16fc57a01f8c2b0e
Jorge <jfgf11@gmail.com>
2023-06-04 21:38:54 -0500

    Adding comments and tests

--- api/test_main.py
+++ api/test_main.py
@@ -5,7 +5,13 @@ import os
 
 client=TestClient(app=app)
 
+
 def test_text_image_generation():
+    """
+    test_text_image_generation that tests the creation of images based on text.
+    It generates a picture of messi and checks if the status of the request is OK and
+    checks if the image of messi was generated, then it deletes the image.
+    """ 
     response = client.get('/?prompt=a picture of messi')
     assert response.status_code == status.HTTP_200_OK
     image_path = os.getcwd() + "/outputs/txt2img-samples/a_picture_of_messi/0.png"
